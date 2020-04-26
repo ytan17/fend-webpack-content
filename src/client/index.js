e3d17d5336df6fc133e955bfa0b8c36a20f92202
Yongshan Tan <yongshantan1024@gmail.com>
2020-04-25 22:14:33 -0500

    Try to set an entry at webpack.config.js. When run "npm run build", it only runs the default entry which is "./src/index.js", but not the one I set - "./src/client/index.js".

--- src/client/index.js
+++ src/client/index.js
@@ -1,7 +1,1 @@
-import { checkForName } from './js/nameChecker'
-import { handleSubmit } from './js/formHandler'
-
-console.log(checkForName);
-
-alert("I EXIST")
-console.log("CHANGE!!");
+alert("src_client_index.js. I EXIST")
---
+++ src/client/webpack.config.js
@@ -0,0 +1,7 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+    // not working, not run the default one -- .src/index.js
+    entry: "./src/client/index.js",
+}
