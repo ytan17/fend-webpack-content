b959fc50783cdd51e65951015c884aa0ea3f01b7
Yongshan Tan <yongshantan1024@gmail.com>
2020-04-26 22:03:50 -0500

    Working version with run "npm run build-prod". Set the entry as './src/client/index.js' instead of using entry './src/index.js'.

--- src/client/webpack.config.js
+++
@@ -1,7 +0,0 @@
-const path = require('path');
-const webpack = require('webpack');
-
-module.exports = {
-    // not working, not run the default one -- .src/index.js
-    entry: "./src/client/index.js",
-}
