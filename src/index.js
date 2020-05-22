6928b3be61b38e2f1e194e2a0338816bf50ef0cf
Yongshan Tan <yongshantan1024@gmail.com>
2020-05-22 11:01:02 -0500

    Add the output in webpack.dev.js with create the Client library to make the JavaScript function working. But still have problem- after click submit btn, no message print out.

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import { checkForName } from './client/js/nameChecker'
-import { handleSubmit } from './client/js/formHandler'
-
-console.log(checkForName);
-
-alert("I EXIST")
-console.log("CHANGE!!");
